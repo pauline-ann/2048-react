9f818c8f5b43dd71c56eee288d70dd98dcf3147a
pauline-ann <paulinebantayan@gmail.com>
2023-03-17 08:24:22 -0700

    convert to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App'
 import './main.scss'
 import './styles.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
