72b24189372c8010c0f1fb7a715a715c47b91a06
pauline-ann <paulinebantayan@gmail.com>
2023-03-16 22:12:13 -0700

    import board view to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import BoardView from './components/BoardView'
 
 function App() {
 
   return (
     <div className="App">
-      <h2>2048</h2>
+      <BoardView />
     </div>
   )
 }
